d7b0c93ebc038e2ab07e3fa3cd88211660fec803
etkirsch <etkirsch@gmail.com>
2015-12-02 08:10:53 -0800

    Added Stations and All Access to gmusicclient

--- gmusic/content/GMusicClient.py
+++ gmusic/content/GMusicClient.py
@@ -15,22 +15,28 @@ class GMusicClient:
         '''Load user's songs, playlists, and stations'''
         self.songs = self.client.get_all_songs()
         self.radios = self.client.get_all_stations()
-        self.playlists = self.client.get_all_playlists()
 
-    def get_radio(self, name):
-        pass
+    def get_radio_contents(self, radio_id):
+        tracks = self.client.get_station_tracks(radio_id)
+        return tracks
 
     def get_radio_list(self, name):
         return self.filter(self.radios, 'name', name)
 
-    def get_playlist(self):
-        pass
+    def filter(self, element, field, filter_by):
+        return [e for e in element if filter_by in e[field]]
 
     def get_playlist_list(self, name):
         return self.filter(self.playlists, 'name', name)
 
-    def search(self):
-        pass
+    def search_all_access(self, query):
+        return self.client.search_all_access(query)
 
-    def filter(self, element, field, filter_by):
-        return [e for e in element if filter_by in e[field]]
+    def search_tracks_all_access(self, query):
+        return self.search_all_access(query)['song_hits']
+
+    def search_artists_all_access(self, query):
+        return self.search_all_access(query)['artist_hits']
+
+    def search_albums_all_access(self, query):
+        return self.search_all_access(query)['album_hits']
