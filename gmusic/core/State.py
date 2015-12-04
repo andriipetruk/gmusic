890270a8c26777572747de6f3f72b434ab400486
etkirsch <etkirsch@gmail.com>
2015-12-04 09:49:28 -0800

    Comments in State; added page num

--- gmusic/core/State.py
+++ gmusic/core/State.py
@@ -16,23 +16,8 @@ class State(object):
         # this just so happens to work in my favor!
         self.main_menu()
 
-    def adjust_selection(self, amount):
-        """Adjusts the position of the selection cursor in the menu"""
-        self.selected_element += amount
-        self.selected_element = self.selected_element % len(self.page_elements)
-
-    def change_page(self, value):
-        new_page = self.constrain_page_number(self.page_number + value)
-        self.set_page(new_page)
-
-    def set_page(self, page):
-        self.selected_element = 0
-        self.page_number = page
-        self.page_elements = self.full_elements[self.page_number]
-        if self.state != 'main' and self.page_elements[-1][0] != 'Back':
-            self.page_elements.append(('Back','back'))
-
     def main_menu(self):
+        '''Set everything necessary for the main menu'''
         self.full_elements = [self.state_aliases]
         self.page_elements = self.state_aliases
         self.state = "main"
@@ -40,7 +25,33 @@ class State(object):
         self.subtitle = "Options"
         self.selected_element = 0
 
+    def adjust_selection(self, amount):
+        """Adjusts the position of the selection cursor in the menu"""
+        self.selected_element += amount
+        self.selected_element = self.selected_element % len(self.page_elements)
+
+    def change_page(self, value):
+        '''Add `value` to the page_number, constrain it, then set page'''
+        new_page = self.constrain_page_number(self.page_number + value)
+        self.set_page(new_page)
+
+    def set_page(self, page):
+        '''Set the page to a specific page number'''
+        self.selected_element = 0
+        self.page_number = page
+        self.page_elements = self.full_elements[self.page_number]
+
+        # Handle Page Number
+        self.title = self.state
+        if len(self.full_elements) > 1:
+            self.title = self.title + ' (Page {0} of {1})'.format(self.page_number+1, len(self.full_elements))
+
+        # If this isn't the main menu, make sure that we have 'BACK' available
+        if self.state != 'main' and self.page_elements[-1][0] != 'Back':
+            self.page_elements.append(('Back','back'))
+
     def handle_execute(self):
+        '''State Machine'''
         if self.state == 'main':
             return self.page_elements[self.selected_element][1]
 
@@ -58,7 +69,6 @@ class State(object):
 
     def set_options(self, new_options, capacity):
         '''Splits the elements up into sublists for pages'''
-        self.title = self.state
         self.full_elements = [new_options[x:x+capacity] \
             for x in range(0, len(new_options), capacity)]
         self.change_page(0)
