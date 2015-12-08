9f9afc912664c4d4261f02cc7cc1b553e390b2f9
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-12-08 09:33:05 -0500

    command input now has a more flexible width / help

--- gmusic/frontend/UI.py
+++ gmusic/frontend/UI.py
@@ -3,6 +3,7 @@ from gmusic.core.CommandParser import CommandParser
 from gmusic.core.EventHandler import EventHandler
 from gmusic.core.UIParser import UIParser
 import curses
+import collections
 
 class UI(CursedObject, EventHandler):
     """Asynchronous. Handles input from the user"""
@@ -33,6 +34,12 @@ class UI(CursedObject, EventHandler):
             self.notify_attachments('PageUpdate')
             return
 
+        # TODO: figure out how to have the UI listen fo the escape key
+        # elif user_input == 27: # 27 is escape
+        #     del self.cli_prompt
+        #     self.screen.refresh()
+        #     return
+
         # Otherwise it goes off to the UI Parser
         self.ui_parser.parse(user_input)
 
@@ -48,21 +55,93 @@ class UI(CursedObject, EventHandler):
         self.cmd_parser.parse(request)
 
     def draw_cli_prompt(self, height, width):
-        win = curses.newwin(14, 60, height-20, int(width*0.5)-30)
+        indentation = 2
+        screen_padding = 20
+        text_padding = 12
+
+        # Content
+        help_options = collections.OrderedDict()
+
+        help_options['album'] =     '> album title     Search for Albums'
+        help_options['artist'] =    '> artist name     Search for Artists'
+        help_options['playlist'] =  '> playlist name   Search for Playlists'
+        help_options['radio'] =     '> radio name      Search for Radios'
+        help_options['song'] =      '> song title      Search for Songs'
+        help_options['play'] =      '> play            Begins Playback'
+        help_options['pause'] =     '> pause           Pauses Playback'
+        help_options['next'] =      '> next            Play Next'
+        help_options['previous'] =  '> previous        Play Previous'
+        help_options['random'] =    '> random          Toggle Shuffle'
+        help_options['back'] =      '> back            Back to Main Menu'
+        help_options['exit'] =      '> exit            Exit the program'
+
+        # Examples for the above (only used if the window is wide enough)
+        examples = {
+            'album': '\t\te.g.:  > album Discovery',
+            'artist': '\t\te.g.:  > artist Daft Punk',
+            # the playlist is a long word
+            # and messes up the tabbing
+            # TODO: see if there is a curses table implementation
+            # where we could just draw to table cells
+            'playlist': '\te.g.:  > playlist Eletro-Ambient',
+            'radio': '\t\te.g.:  > radio Armin',
+            'song': '\t\te.g.:  > song aerodynamic'
+        }
+
+        # calculate the longest line of text
+        # this will be used to compare with the window width to see
+        # if we even can add the examples
+        longest_core_option_length = len(max(help_options.values(), key=len))
+        longest_example_length = len(max(examples.values(), key=len))
+        longest_help_text = longest_core_option_length + longest_example_length
+
+        # Height is based off the content of the cli
+        # + a blank line
+        # + a line for input
+        # + the top and bottom borders (2)
+        window_height = len(help_options) + 1 + 1 + 2
+        window_width = width - (screen_padding * 2)
+
+        # determine if the window width should be shorter
+        if window_width < (longest_help_text + screen_padding * 2):
+            # reduce width to fit just the help
+            window_width = longest_core_option_length + text_padding
+        else:
+            # window is plenty wide, add the examples
+            window_width = longest_help_text + text_padding
+            for command, text in examples.items():
+                help_options[command] = help_options[command] + text
+
+        # Height of this window with some padding underneath
+        begin_y = height - (window_height + 4)
+        # half the width of the screen minus half the width of the window
+        begin_x = int(width * 0.5) - (window_width / 2)
+
+        # Now that we have all our parameters for constraining the window,
+        # create the window
+        self.cli_prompt = win = curses.newwin(window_height, window_width, begin_y, begin_x)
+
         win.box()
         win_h, win_w = win.getmaxyx()
-        win.addstr(0, 2, 'Commands', curses.A_BOLD)
-        win.addstr(1, 2, '"album ____"       Search for Albums', curses.A_DIM)
-        win.addstr(2, 2, '"artist ____"      Search for Artists', curses.A_DIM)
-        win.addstr(3, 2, '"playlist ____"    Search for Playlists', curses.A_DIM)
-        win.addstr(4, 2, '"radio ____"       Search for Radios', curses.A_DIM)
-        win.addstr(5, 2, '"song ____"        Search for Songs', curses.A_DIM)
-        win.addstr(6, 2, '"play"/"pause"     General Playback', curses.A_DIM)
-        win.addstr(7, 2, '"next"/"previous"  Play Next or Previous', curses.A_DIM)
-        win.addstr(8, 2, '"random"           Toggle Shuffle', curses.A_DIM)
-        win.addstr(9, 2, '"back"             Back to Main Menu', curses.A_DIM)
-        win.addstr(10, 2, '"exit"             Exit the program', curses.A_DIM)
-        win.addstr(win_h-2, 2, "> ")
+
+        # The first line of text will be at this height from the top of the window
+        line_index = 1
+        last_line_index = window_height - 2
+        # Title
+        win.addstr(0, indentation, 'Commands', curses.A_BOLD)
+
+        # Add the text to the window
+        for command, text in help_options.items():
+            win.addstr(line_index, indentation, text, curses.A_DIM)
+            line_index += 1
+
+        #win.addstr(14, 2, 'ESC will close this modal', curses.A_DIM)
+        win.addstr(line_index, indentation, '', curses.A_DIM)
+        win.addstr(last_line_index, indentation, "> ")
+
+        # draw everything
         self.screen.refresh()
         win.refresh()
-        return win.getstr(win_h-2, 4, win_w-4)
+
+        # listen for commands
+        return win.getstr(last_line_index, indentation * 2, win_w-(indentation * 2))
