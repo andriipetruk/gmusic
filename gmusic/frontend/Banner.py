7ed99105f83985c1a8b55df81f346c8ae1865f59
etkirsch <etkirsch@gmail.com>
2015-12-16 07:57:47 -0800

    moved banner up a row

--- gmusic/frontend/Banner.py
+++ gmusic/frontend/Banner.py
@@ -8,22 +8,23 @@ class Banner(CursedObject):
     def __init__(self, screen):
         self.screen = screen
         self.track = None
+        self.start_row = 1
 
     def draw_no_track(self):
         """Draws a blank banner if there's not a song playing"""
-        self.center_text('  ', 2)
-        self.center_text("Google Music Terminal", 3, curses.A_BOLD)
-        self.center_text('  ', 4)
+        self.center_text('  ', self.start_row)
+        self.center_text("Google Music Terminal", self.start_row+1, curses.A_BOLD)
+        self.center_text('  ', self.start_row+2)
 
     def draw_track_details(self, is_playing=True, is_random=False):
         """Draws a banner for the current track if it is playing"""
         title_string = self.track['title']
         if not is_playing:
-            title_string = '▌▌ ' + title_string 
+            title_string = '▌▌ ' + title_string
 
-        self.center_text(title_string, 2, curses.A_BOLD)
-        self.center_text(self.track['album'], 3)
-        self.center_text(self.track['artist'], 4)
+        self.center_text(title_string, self.start_row, curses.A_BOLD)
+        self.center_text(self.track['album'], self.start_row+1)
+        self.center_text(self.track['artist'], self.start_row+2)
 
     def draw(self, is_playing=True, is_random=False):
         """Master draw method, checks the new_track to see what to draw"""
