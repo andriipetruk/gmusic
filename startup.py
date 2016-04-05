698dbc4ba998227422151ba393b336e208972c34
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-04-05 17:32:10 -0400

    omg, python, pls

--- startup.py
+++ startup.py
@@ -1,20 +1,42 @@
-from gmusic.core.Core import Core
 import os, json, subprocess, sys
 
+# Check Dependencies
+check_gst_command = 'dpkg -l python-gst-1.0'
+check_gmusicapi_command = 'pip list | grep gmusicapi'
+check_pygobject_command = 'pip list | grep pygobject'
 
-pip_command = 'pip install -r requirements.txt'
-gstreamer_command = 'apt-get install python-gst-1.0'
+gst_result = subprocess.check_output(check_gst_command.split(" "))
+gmusicapi_result = subprocess.check_output(check_gmusicapi_command.split(" "))
+pygobject_result  = subprocess.check_output(check_pygobject_command.split(" "))
+
+# TODO: what if non-english system?
+gst_missing = ("no packages found" in gst_result)
+gmusicapi_missing = (not gmusicapi_result)
+pygobject_missing = (not pygobject_result)
+
+if gst_missing:
+    print('It looks like python-gst-1.0 was not found on your system.')
+    print('You can install them with this command\n')
+    print('\033[93m\tsudo apt-get install python-gst-1.0')
+
+if gmusicapi_missing or pygobject_missing:
+    print('It looks like you don\'t have all the pip requirements.')
+    print('You can install them with this command\n')
+    print('\033[93m\tsudo pip install -r requirements.txt --no-cache-dir')
+
+print(gst_missing)
+print(gmusicapi_missing)
+print(pygobject_missing)
+
+if gst_missing or gmusicapi_missing or pygobject_missing:
+    sys.exit(1)
+
+# now that we have all the dependency shenanigans out of the way,
+# load up the app
+from gmusic.core.Core import Core
+
 
 if not os.path.isfile('data/unlocked/credentials.json'):
-    try:
-        output = subprocess.check_output(['bash', '-c', pip_command])
-        output = subprocess.check_output(['bash', '-c', gstreamer_command])
-    except:
-        print('Dependencies not installed, please run as sudo.')
-        sys.exit(1)
-
-    print('Installed dependencies.')
-
     print('Welcome to GMusic, a Bash Terminal implementation of Google All-Access Music.')
     print('Please enter an application password for Google Music. This will be stored')
     print('locally on your device. WE WILL NEVER ACCESS THIS FILE OUTSIDE OF USE IN')
